06cf41a78c42e6909d02ebb9f37c63beae932a09
Mohnish Kalia <mohnish.kalia@gmail.com>
2021-04-03 03:50:44 -0500

    Working setup and prisma

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import Head from 'next/head'
 
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -11,98 +11,70 @@ import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/sty
 
 import Header from '../components/Header';
 import Autocomplete from '@material-ui/lab/Autocomplete';
+import { GetStaticProps } from 'next';
+import { PrismaClient } from '@prisma/client';
+
+interface SQLOption {
+	id: number;
+	name: string;
+	type: number;
+}
 
 interface Option {
-    id: number;
-    name: string;
+	id: number;
+	name: string;
+	type: string;
 }
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-        marginBottom: theme.spacing(2)
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    title: {
-        flexGrow: 1,
-    },
-    option: {
-        fontSize: 15,
-        '& > span': {
-          marginRight: 10,
-          fontSize: 18,
-        },
-    },
+	root: {
+		flexGrow: 1,
+		marginBottom: theme.spacing(2)
+	},
+	menuButton: {
+		marginRight: theme.spacing(2),
+	},
+	title: {
+		flexGrow: 1,
+	},
+	option: {
+		fontSize: 15,
+		'& > span': {
+			marginRight: 10,
+			fontSize: 18,
+		},
+	},
 }));
 
-const options = [{
-	id: 10000,
-	name: 'Ten Thousand Dragon'
-}, {
-	id: 27551,
-	name: 'Limit Reverse'
-}, {
-	id: 32864,
-	name: 'The 13th Grave'
-}, {
-	id: 35699,
-	name: 'SPYRAL Sleeper'
-}, {
-	id: 39015,
-	name: 'Assault Sentinel'
-}, {
-	id: 41546,
-	name: 'D/D Savant Thomas'
-}, {
-	id: 41777,
-	name: 'Gem-Enhancement'
-}, {
-	id: 50755,
-	name: "Dark Magician's Circle"
-}, {
-	id: 56889,
-	name: 'Cross-Dimensional Duel'
-}, {
-	id: 62121,
-	name: 'Castle of Dark Illusions'
-}, {
-	id: 96540,
-	name: 'Downbeat'
-}, {
-	id: 98905,
-	name: 'Cipher Spectrum'
-}] as Option[]
+export default function Home({ options }: { options: Option[] }) {
+	const [id, setId] = useState(0);
 
-export default function Home() {
 	const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
 	const classes = useStyles();
 
-    const theme = useMemo(
-        () =>
-            createMuiTheme({
-                palette: {
-                    type: prefersDarkMode ? 'dark' : 'light',
-                },
-            }),
-        [prefersDarkMode],
-    );
-	
+	const theme = useMemo(
+		() =>
+			createMuiTheme({
+				palette: {
+					type: prefersDarkMode ? 'dark' : 'light',
+				},
+			}),
+		[prefersDarkMode],
+	);
+
 	return (
 		<ThemeProvider theme={theme}>
 			<Head>
-				<title>Create Next App</title>
+				<title>Simple Descriptions</title>
 				<link rel="icon" href="/favicon.ico" />
 				<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
 			</Head>
 			<CssBaseline />
-			<Header/>
+			<Header />
 			<Container maxWidth="lg">
 				<Grid container spacing={3} direction="column">
 					<Grid container item alignItems="center">
-						<Grid item md={3}>
-						</Grid>
+						<Grid item md={3}></Grid>
 						<Grid item xs={12} md={6}>
 							<Autocomplete
 								id="card-select"
@@ -110,11 +82,13 @@ export default function Home() {
 								classes={{
 									option: classes.option,
 								}}
+								groupBy={(option) => option.type}
 								autoHighlight
+								onChange={(_, newValue: Option | null) => {
+									if (newValue)
+										setId(newValue.id);
+								}}
 								getOptionLabel={(option) => option.name}
-								renderOption={(option) => (
-									<span>{option.name}</span>
-								)}
 								renderInput={(params) => (
 									<TextField
 										{...params}
@@ -128,9 +102,39 @@ export default function Home() {
 								)}
 							/>
 						</Grid>
+						<Grid item md={3}></Grid>
+
+						<Grid item md={3}></Grid>
+						<Grid container item xs={12} md={6}>
+							<Typography variant="h3">{(id).toString().padStart(8, '0')}</Typography>
+						</Grid>
+						<Grid item md={3}></Grid>
 					</Grid>
 				</Grid>
 			</Container>
 		</ThemeProvider>
 	)
+}
+
+// https://db.ygoprodeck.com/api/v7/cardinfo.php?id=03298689
+
+export const getStaticProps: GetStaticProps = async (context) => {
+	const prisma = new PrismaClient();
+	const results: SQLOption[] = await prisma.$queryRaw`
+		SELECT d.id, t.name, d.type from texts t
+		inner join datas d
+			on t.id = d.id
+		`;
+	const options: Option[] = results.map((option) => {
+		let type = '';
+		if ((option.type & 1) === 1) type = 'Monster';
+		if ((option.type & 2) === 2) type = 'Spell';
+		if ((option.type & 4) === 4) type = 'Trap';
+		if ((option.type & 8) === 8) type = 'N/A';
+
+		return {...option, type};
+	}).sort((a, b) => -b.type.localeCompare(a.type));
+	return {
+		props: { options }
+	}
 }
