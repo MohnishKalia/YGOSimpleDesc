62e98437557bed3fa3fdb0a47678555b5cb07dd6
Mohnish Kalia <mohnish.kalia@gmail.com>
2021-04-04 18:44:27 -0500

    Working display

--- pages/api/search/[passcode].ts
+++ pages/api/search/[passcode].ts
@@ -1,15 +1,35 @@
-import { NextApiRequest, NextApiResponse } from "next"
+import { PrismaClient } from '@prisma/client';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const prisma = new PrismaClient();
+
+export interface SearchResponse {
+    id: number,
+    name: string,
+    desc: string,
+    sdesc: string,
+    url: string,
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method !== 'GET') return res.status(404).json({ error: 'Invalid method.' });
-    
+
     const { passcode } = req.query;
-    if (!passcode || typeof passcode !== 'string') return res.status(409).json({ error: 'Invalid passcode.' });
+    if (!passcode || typeof passcode !== 'string') 
+        return res.status(409).json({ error: 'Invalid passcode format.' });
 
     const response = await fetch(`https://db.ygoprodeck.com/api/v7/cardinfo.php?id=${passcode}`);
-    if (!response.ok) return res.status(response.status).json({error: 'Problem fetching from card image api.'});
-
+    if (!response.ok)
+        return res.status(response.status).json({ error: 'Problem fetching from card image api, passcode may be invalid.' });
+    
     const json = await response.json();
 
-    res.status(200).json(json);
+    const card = (await prisma.$queryRaw`
+        SELECT d.id, t.name, t.desc, t.sdesc from texts t
+            inner join datas d
+                on t.id = d.id
+            where d.id = ${passcode}
+        `)[0];
+
+    res.status(200).json({ ...card, url: json.data[0].card_images[0].image_url } as SearchResponse);
 }
--- pages/search/[passcode].tsx
+++ pages/search/[passcode].tsx
@@ -1,13 +1,20 @@
-import Typography from '@material-ui/core/Typography';
 import AppBar from '@material-ui/core/AppBar';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
 import Tab from '@material-ui/core/Tab';
 import Tabs from '@material-ui/core/Tabs';
-import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import ArrowBack from '@material-ui/icons/ArrowBack';
+import Alert from '@material-ui/lab/Alert';
+import Image from 'next/image';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
-import useSWR from 'swr';
 import { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Image from 'next/image'
+import useSWR from 'swr';
+import type { SearchResponse } from '../api/search/[passcode]';
 
 interface TabPanelProps {
     children?: React.ReactNode;
@@ -40,7 +47,23 @@ const useStyles = makeStyles((theme) => ({
         flexGrow: 1,
         backgroundColor: theme.palette.background.paper,
     },
+    center: {
+        textAlign: 'center',
+        paddingTop: theme.spacing(1),
+        paddingBottom: theme.spacing(1),
+    },
+    button: {
+        marginBottom: theme.spacing(1),
+    },
 }));
+
+const fetcher = async (url: string) => {
+    const res = await fetch(url);
+    const json = await res.json();
+    if (!res.ok)
+        throw new Error(json.error);
+    return json;
+}
 
 export default function Passcode() {
     const router = useRouter();
@@ -48,34 +71,56 @@ export default function Passcode() {
     const { passcode } = router.query;
     const [value, setValue] = useState(0);
 
-    const { data, error } = useSWR(`/api/search/${passcode}`, async (input: RequestInfo, init?: RequestInit) => {
-        const res = await fetch(input, init);
-        return res.json();
-    });
+    const { data, error } = useSWR<SearchResponse, Error>(`/api/search/${passcode}`, fetcher);
 
-    if (!data) return <span>loading</span>;
-    if (error) return <span>error!</span>;
+    if (error) return <Alert severity="error">{error.message}</Alert>;
+    if (!data) return <LinearProgress />;
 
     return (
-        <div className={classes.root}>
-            <Typography variant="h4">The passcode is {passcode}</Typography>
-            <AppBar position="static">
-                <Tabs value={value} onChange={(_, newValue) => setValue(newValue)} aria-label="simple tabs example">
-                    <Tab label="Item One" />
-                    <Tab label="Item Two" />
-                </Tabs>
-            </AppBar>
-            <TabPanel value={value} index={0}>
-                {data && <Image
-                    src={data[0].card_images[0].image_url as string}
-                    alt="Picture of the author"
-                    width={500}
-                    height={500}
-                />}
-            </TabPanel>
-            <TabPanel value={value} index={1}>
-                Item Two
-            </TabPanel>
-        </div >
+        <>
+            <Link href="/">
+                <Button
+                    variant="contained"
+                    color="default"
+                    className={classes.button}
+                    startIcon={<ArrowBack />}
+                >
+                    Go Back
+                </Button>
+            </Link>
+            <Paper elevation={0}>
+                <AppBar position="static">
+                    <Typography variant="button" className={classes.center}>{data.name}</Typography>
+                    <Tabs
+                        value={value}
+                        onChange={(_, newValue) => setValue(newValue)}
+                        aria-label="description selection"
+                        variant="fullWidth"
+                        centered
+                    >
+                        <Tab label="Simplified" />
+                        <Tab label="Actual" />
+                        <Tab label="Card Image" />
+                    </Tabs>
+                </AppBar>
+                <TabPanel value={value} index={0}>
+                    {data.sdesc
+                        ? <Typography variant="body2">{data.sdesc}</Typography>
+                        : <Alert severity="info">There currently is no simplified description for this card.</Alert>
+                    }
+                </TabPanel>
+                <TabPanel value={value} index={1}>
+                    <Typography variant="body2">{data.desc}</Typography>
+                </TabPanel>
+                <TabPanel value={value} index={2}>
+                    <Image
+                        src={data.url}
+                        alt="card image"
+                        width={420}
+                        height={615}
+                    />
+                </TabPanel>
+            </Paper>
+        </>
     );
 }
